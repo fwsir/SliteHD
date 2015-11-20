5a4a4151e6465471d05b7486f8b95ee3b4ae118a
fwsir <LiNing@Mac-mini.local>
2015-11-20 19:32:52 +0800

    SliteHD
    hddddddddd.

---
+++ SliteHD/src/ui/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SliteHD
+//
+//  Created by LiNing on 15/11/20.
+//  Copyright © 2015年 HIKVISION. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
